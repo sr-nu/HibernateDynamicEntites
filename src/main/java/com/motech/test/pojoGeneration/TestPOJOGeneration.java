8ba77aed8819afd5af7b761a0cce74b43bed550f
Srikanth <srikanth.nutigattu@gmail.com>
2013-07-12 17:00:01 +0530

    Refactoring

--- src/main/java/com/motech/test/pojoGeneration/TestPOJOGeneration.java
+++ src/main/java/com/motech/test/pojoGeneration/TestPOJOGeneration.java
@@ -14,33 +14,30 @@ import java.util.Map;
 
 public class TestPOJOGeneration {
 
-    private static String tableName;
+    private static String tableName = "NEW_PERSON";
 
     public static void main(String[] args) throws NotFoundException, CannotCompileException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Map<String, Class<?>> props = new HashMap<String, Class<?>>();
         props.put("id", Integer.class);
         props.put("user", String.class);
 
-        Class person = PojoGenerator.generate("com.motech.test.pojoGeneration.NewPerson", props);
-        System.out.println(person);
-        System.out.println(person.getName());
+        Class newPersonClass = PojoGenerator.generate("com.motech.test.pojoGeneration.NewPerson", props);
+        Object newPersonInstance = newPersonClass.newInstance();
+        newPersonClass.getMethod("setId", Integer.class).invoke(newPersonInstance, 34);
+        newPersonClass.getMethod("setUser", String.class).invoke(newPersonInstance,"testUser2");
 
 
-        tableName = "NEW_PERSON";
-        SessionFactory sessionFactory = new CustomEntityCreator().getSessionFactory(person.getName(), tableName);
+        SessionFactory sessionFactory = new CustomEntityCreator().getSessionFactory(newPersonClass.getName(), tableName);
         Session session = sessionFactory.openSession();
-        Object personInstance = person.newInstance();
 
-        person.getMethod("setId",Integer.class).invoke(personInstance, 34);
-        person.getMethod("setUser",String.class).invoke(personInstance,"testUser2");
         Transaction transaction = session.beginTransaction();
-        session.save(personInstance);
+        session.save(newPersonInstance);
         transaction.commit();
 
         Session session1 = sessionFactory.openSession();
         List list = session1.createSQLQuery("select * from "+tableName).list();
 
-        System.out.println("Number of entities:"+ list.size());
+        System.out.println("Number of entities:" + list.size());
 
         for (Object o : list) {
             System.out.println(o);
@@ -49,9 +46,9 @@ public class TestPOJOGeneration {
         session1.close();
 
         Session session2 = sessionFactory.openSession();
-        List list1 = session2.createQuery("From "+person.getName()).list();
+        List list1 = session2.createQuery("From "+newPersonClass.getName()).list();
         for (Object o : list1) {
-            System.out.println(person.getMethod("getId").invoke(o)+ " :: " + person.getMethod("getUser").invoke(o));
+            System.out.println(newPersonClass.getMethod("getId").invoke(o)+ " :: " + newPersonClass.getMethod("getUser").invoke(o));
         }
 
 
